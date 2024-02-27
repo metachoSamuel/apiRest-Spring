6be11f2dbea679221b55c063059d3721cbdb8b98
metachoSamuel <samuel.f.met@gmail.com>
2024-02-26 21:23:59 -0500

    Intial commit

---
+++ src/main/java/com/metacho/apirest_spring/ApirestSpringApplication.java
@@ -0,0 +1,13 @@
+package com.metacho.apirest_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApirestSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApirestSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/metacho/apirest_spring/ApirestSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.metacho.apirest_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApirestSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
